055b3f8b742fe6963b872188e2bcedad779ddfc5
ryan knight <ryan.knight@evolvemep.com>
2022-03-10 18:07:13 -0800

    initial commit

---
+++ apps/advice-generator/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'advice-generator',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/advice-generator',
+};
